02965238a80af2d88d79bb321d9a543ccd36658d
Crstt <114186289+Crstt@users.noreply.github.com>
2023-07-04 16:50:02 +0000

    adds git ignore, first draft index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+const dbURI = "mongodb:";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
