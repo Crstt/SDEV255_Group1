c503fd73b87a922cae0ae6b3ce1d0aa211d2ae64
Crstt <matteo598.catalano@gmail.com>
2023-07-04 16:26:45 -0400

    Adds: MongoDB integration Index.js routes nav bootstrap create delete details

---
+++ classes/options.js
@@ -0,0 +1,26 @@
+const fs = require('fs'); 
+
+class Options {
+  constructor(file_path) {
+    this.user = null;
+    this.password = null;
+    this.readOptionsFile(file_path);
+  }
+
+  readOptionsFile(file_path) {
+    const file = fs.readFileSync(file_path, 'utf8');
+    const lines = file.split('\n');
+
+    for (let line of lines) {
+      const [key, value] = line.trim().split('=');
+
+      if (key === 'user') {
+        this.user = value;
+      } else if (key === 'password') {
+        this.password = value;
+      }
+    }
+  }
+}
+
+module.exports = Options;
